c826424ccc8f92f471df4449894d0866db15bdd2
maironcuello <mmairon@gmail.com>
2023-05-03 20:34:54 -0500

    create: dto and entity for create products

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,42 @@
-export class CreateProductDto {}
+import {
+  IsArray,
+  IsIn,
+  IsInt,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+// yarn add class-validator class-transformer
+export class CreateProductDto {
+  @IsString()
+  @MinLength(1)
+  title: string;
+
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  price?: number;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsString()
+  @IsOptional()
+  slug?: string;
+
+  @IsInt()
+  @IsPositive()
+  @IsOptional()
+  stock?: number;
+
+  @IsString({ each: true })
+  @IsArray()
+  sizes: string[];
+
+  @IsIn(['men', 'wonan', 'kid', 'unisex'])
+  gerder: string;
+}
--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -7,4 +7,23 @@ export class Product {
 
   @Column('text', { unique: true })
   title: string;
+
+  @Column('numeric', { default: 0 })
+  price: number;
+
+  // nullable: can get null
+  @Column({ type: 'text', nullable: true })
+  description: string;
+
+  @Column('text', { unique: true })
+  slug: string;
+
+  @Column('int', { default: 0 })
+  stock: number;
+
+  @Column('text', { array: true })
+  sizes: string[];
+
+  @Column('text')
+  gender: string;
 }
